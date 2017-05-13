c15fed3943f98553435484b1eba457fb09a43d14
eric_tu <eric_tu@localhost.localdomain>
2017-05-13 08:58:17 +0800

    [Feature] luckynumber render with twin by controller

---
+++ src/AppBundle/Controller/LakiController.php
@@ -0,0 +1,25 @@
+<?php
+
+// src/AppBundle/Controller/LakiController.php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LakiController extends Controller
+{
+    /**
+     * @Route("/laki/number")
+     */
+    public function numberAction()
+    {
+        $number = mt_rand(0, 100);
+
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
+    }
+}
+
+
